3ee40fd8ade590d9bd1181cd21ae2dc4c23cfcc9
mkudlacz@rim.com <mkudlacz@rim.com>
2011-05-04 12:33:20 -0400

    finished html_media.js base definition updated audio and video added source and track elements

--- api/html5_media_audio.js
+++ api/html5_media_audio.js
@@ -14,180 +14,27 @@
 * limitations under the License.
 */
 
+/*
+* Taken from:
+* http://www.w3.org/TR/html5/video.html
+*/
+
 /**
- * @toc {Media} HTML5 Audio
- * @PB10
- * @BB60+
- * @class audio
- * The audio tag defines sound, such as music or other audio streams.
- * <p/>
- * Tips and Notes:
- * Any text inside the between audio tag will be displayed in browsers that does not support the audio element.
- */
-
-audio ={
-
-		/**
-		 * @PB10
-		 * @BB60+
-		 * @description The autoplay attribute specifies that the audio will start playing as soon as it is ready.
-		 * <p/>
-		 * If this attribute is present the audio will autoplay.
-		 * <p>
-		 * syntax :
-		 * &#60audio autoplay="autoplay" /&#62	
-		 * @type {String} [autoplay=autoplay]
-		 * @example
-		 * &#60audio controls="controls" autoplay="autoplay"&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" /&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" /&#62
-		 * Your browser does not support the audio element.
-		 * &#60/audio&#62
-		 * <p>
-		 * <audio controls="controls" autoplay="autoplay">
-		 * <source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" />
-		 * <source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" />
-		 * Your browser does not support the audio element.
-		 * </audio>
-		 */
-		autoplay :,
-		
-		
-		/**
-		 * @PB10
-		 * @BB60+
-		 * @description The controls attribute specifies that the browser should provide playback controls for the audio.
-		 * <p>
-		 * When this attribute is set, it specifies that no scripted controls are set, and the browser should use its default controls.
-		 * <p>
-		 * The browser controls should include: Play, Pause, Seeking, Volume, Fullscreen toggle, Captions/Subtitles (when available) Audio track (when available). 
-		 * <p>
-		 * syntax :
-		 * &#60audio controls="controls" /&#62
-		 * @type {String} [control=control]
-		 * @example
-		 * &#60audio controls="controls"&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" /&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" /&#62
-		 * Your browser does not support the audio element.
-		 * &#60/audio&#62
-		 * <p>
-		 * <audio controls="controls">
-		 * <source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" />
-		 * <source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" />
-		 * Your browser does not support the audio element.
-		 * </audio>
-		 */
-		controls :,
-		
-		
-		/**
-		 * @PB10
-		 * @BB60+
-		 * @description The loop attribute specifies that the audio will start over again, every time it is finished.
-		 * <p>
-		 * If this attribute is present the audio will loop.
-		 * <p/> 
-		 * syntax :
-		 * &#60audio loop="loop" /&#62
-		 * @type {String} [loop=loop] 
-		 * @example
-		 * &#60audio controls="controls" loop="loop"&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" /&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" /&#62
-		 * Your browser does not support the audio element.
-		 * &#60/audio&#62
-		 * <p>
-		 * <audio controls="controls" loop="loop">
-		 * <source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" />
-		 * <source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" />
-		 * Your browser does not support the audio element.
-		 * </audio>
-		 */
-		loop ,
-		
-		/**
-		 * @PB10
-		 * @BB60+
-		 * @description The preload attribute specifies if the audio should be loaded when the page loads.
-		 * <p/> This attribute is ignored if autoplay is present.
-		 * syntax :
-		 * &#60audio preload="value" /&#62
-		 * <p>
-		 * auto :	Specifies that the browser should load entire audio when page loads<br>
-		 * metadata: Specifies that the browser should load only metadata when page loads<br>
-		 * none: Specified that the browser should not pre-load audio when page loads<br>
-		 * @type {String} [preload=auto]<p> [preload=metadata]<p> [preload=none]
-		 * @example
-		 * &#60audio controls="controls" preload="auto"&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" /&#62
-		 * &#60source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" /&#62
-		 * Your browser does not support the audio element.
-		 * &#60/audio&#62
-		 * <p>
-		 * <audio controls="controls" preload="auto">
-		 * <source src="http://www.w3schools.com/html5/song.ogg" type="audio/ogg" />
-		 * <source src="http://www.w3schools.com/html5/song.mp3" type="audio/mpeg" />
-		 * Your browser does not support the audio element.
-		 * </audio>
-		 */
-		preload : ,
-		
-		/**
-		 * @PB10
-		 * @BB60+
-		 * @description The src attribute specifies the URL of the audio to play.
-		 * <p/>The audio can also be set using the <source> tag.
-		 * <p>
-		 * syntax :
-		 * &#60audio src="value"&#62
-		 * <p>
-		 * The URL of the audio file.<br>
-		 * Possible values:<br>
-		 * An absolute URL - points to another web site (like href="http://www.example.com/song.ogg")<br>
-		 * A relative URL - points to a file within a web site (like href="song.ogg")<br>
-		 * 
-		 * @type  {String} [src=URL]
-		 * @example
-		 * &#60audio src="http://www.w3schools.com/html5/song.ogg" controls="controls"&#62
-		 * Your browser does not support the audio element.
-		 * &#60/audio&#62
-		 * <p>
-		 * <audio src="http://www.w3schools.com/html5/
-		 song.ogg" controls="controls">
-		 * Your browser does not support the audio element.
-		 * </audio>
-		 */
-		src : ,
-};
-
-  /**
-   * This class provides access to the device media, interfaces to both sound and video
-   * @constructor
-   */
-   function Media(src) {
-  	this.src = src;
-  }
-  
-  /**
-   * [readonly] Returns true if playback is paused; false otherwise.
-   * @property {Boolean}
-   */
-   Media.paused : ,
-  
-  /**
-  * Causes the media player to start playing.
-  */
-  Media.prototype.play = function() {
-  }
- /**
-  * Pauses the playing of the media player. Note: Pausing of a live stream is not supported.
-  */
- Media.prototype.pause = function() {
-  }
-  /**
-   * Stops the playing of the media player.
-   */
-  Media.prototype.stop = function() {
-  }
-  
+* An audio element represents a sound or audio stream.
+* @toc {Media} HTML5 Audio
+* @class 
+* @extends HTMLMediaElement
+* @PB10
+* @BB60+
+* @example
+* var audio = document.createElement('audio');
+* audio.src = 'audio.wav';
+* audio.controls = true;
+* document.body.appendChild(audio);
+* @example
+* var audio = new Audio();
+* audio.src = 'audio.wav';
+* @example
+* var audio = new Audio('audio.wav');
+*/
+HTMLAudioElement = { };
