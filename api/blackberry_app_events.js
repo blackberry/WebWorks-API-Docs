f7385e025d3c8277de19d7cd04967cba04362ca2
Carolina Pinzon <cpinzon@rim.com>
2011-05-10 18:33:37 -0400

    Several small fixes

--- api/blackberry_app_events.js
+++ api/blackberry_app_events.js
@@ -20,18 +20,6 @@
  * @namespace The Application Event object allows you to access events triggered by the application
   * <p/>
  * The Application Event Object is static; all of its functions and properties are used directly from the object.
- * @example
- * &lt;script type="text/javascript"&gt;
- * function onBackgroundCallback() {
- * 	alert("Going to the background!");
- * }
- *
- * function notifyMeWhenMinimized() {
- * 	blackberry.app.event.onBackground(onBackgroundCallback);
- * }
- * 
- * notifyMeWhenMinimized();
- * &lt;/script&gt;
  * @learns {Sample - Swipe Down Menu} http://supportforums.blackberry.com/t5/Web-and-WebWorks-Development/Sample-Code-Swipe-down-menu-for-BlackBerry-WebWorks-SDK-for/ta-p/943521  Sample that demonstrates how to add a swipe-down menu to your PlayBook application using the onSwipeDown function [BlackBerry Developer Resource Center]. 
  */
 blackberry.app.event = {
@@ -45,6 +33,18 @@ blackberry.app.event = {
 	 * @callback {function} onBackgroundCallback Function to be called when the application goes to the background. Expected signature: function onBackgroundCallback().
 	 * @PB10 
 	 * @BB50+
+	 * @example
+	 * &lt;script type="text/javascript"&gt;
+	 * function onBackgroundCallback() {
+	 * 	alert("Going to the background!");
+	 * }
+	 *
+	 * function notifyMeWhenMinimized() {
+	 * 	blackberry.app.event.onBackground(onBackgroundCallback);
+	 * }
+	 * 
+	 * notifyMeWhenMinimized();
+	 * &lt;/script&gt;
 	 */
 	onBackground : function(onBackgroundCallBack){},
 	
@@ -53,7 +53,7 @@ blackberry.app.event = {
 	 * @description Assigns a listener that is called when the application goes to the foreground.
 	 * <p/>
 	 * This event is dispatched whenever the application comes to the foreground, for example, when the user re-opens the application from the application list. Only one function can be assigned to this event. To unregister the callback call the onForeground method and pass in null for the callback parameter.
-	 * <p/>Note: The BlackBerry PlayBook's "Application Behavior" setting has an impact on when onForeground gets dispatched. Refer to onBackground documentation for details.	
+	 * <p/>Note: The BlackBerry PlayBook's "Application Behavior" setting has an impact on when onForeground gets dispatched. Refer to {@link blackberry.app.event.onBackground} documentation for details.	
 	 * @callback {function} onForegroundCallback Function to be called when the application goes to the foreground. Expected signature: function onForegroundCallback().
 	 * @PB10
 	 * @BB50+
