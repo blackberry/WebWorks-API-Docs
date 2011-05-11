ba7285a160bed553a279d9d2cf9a8eab42b9bbd1
Jack Luo <jluo@rim.com>
2011-05-11 11:05:58 -0400

    various fixes: add @throws tag, chang DOMString to String, add more sample codes in localStorage, use @constructedBy in database, change @desc to @description

--- api/html5_database.js
+++ api/html5_database.js
@@ -15,28 +15,30 @@
 */
 
 /**
+ * @PB10
+ * @BB50+
  * @toc {Data Storage} HTML5 Database
  * @namespace This object provides functions to manipulate client-side databases using SQL. 
  * <p/>
- * The <b>openDatabase()</b> method on the <b>Window</b> can be used to get an instance of database. For example:<br/>
- * <i>Database db=Window.openDatabase('documents', '1.0', 'Offline document storage', 5*1024*1024, null);</i>
- * <br/><br/>
  * <b>Important Note:</b> The HTML5 Database object is marked as supported for OS 5.0.  This support is accomplished by using the <a href="http://supportforums.blackberry.com/t5/Web-and-WebWorks-Development/Supporting-Gears-using-HTML5-in-BlackBerry-WebWorks-applications/ta-p/557280" target="_blank">HTML5 JavaScript toolkit</a> for BlackBerry OS 5.0.
- * @PB10
- * @BB50+
+ * @constructedBy {Window.openDatabase(String name, String version, String displayName, Number estimatedSize, function creationCallback)} {You can use Window.openDatabase method to get an instance of database. This method has five parameters: <i>name</i> is a database name; <i>version</i> is a database version; <i>displayName</i> is a display name; <i>estimatedSize</i> is an estimated size -in bytes- of the data that will be stored in the database; and <i>creationCallback</i> is optional callback function and is invoked if the database has not yet been created. Expected callback function signature: function DatabaseCallback(Database database).}
+ * // This is an example of how to get an instance of Database.
+ * Database db=Window.openDatabase('documents', '1.0', 'Offline document storage', 5*1024*1024, null);
+ * 
+ * 
  */
 Database ={
 		/**
-		 * @desc the current version of the database.
+		 * @description the current version of the database.
 		 * @readOnly
-		 * @type DOMString
+		 * @type String
 		 * @PB10
 		 * @BB50+
 		 */
 		version : "",
 		
 		/**
-		 * @desc When called, this method immediately returns and then asynchronously runs the transaction steps with the <i>transaction callback</i> being the first argument, the <i>error callback</i> being the second argument, if any, the <i>success callback</i> being the third argument, if any, and with no <i>preflight operation</i> or <i>postflight operation</i>. The mode is read/write.
+		 * @description When called, this method immediately returns and then asynchronously runs the transaction steps with the <i>transaction callback</i> being the first argument, the <i>error callback</i> being the second argument, if any, the <i>success callback</i> being the third argument, if any, and with no <i>preflight operation</i> or <i>postflight operation</i>. The mode is read/write.
 		 * @callback {function} callback Function to be called when executing SQL statements. Expected signature: function SQLTransactionCallback(SQLTransaction transaction).
 		 * @callback {function} [errorCallback] Function to be called when SQL error occurs. Expected signature: function SQLTransactionErrorCallback(SQLError error).
 		 * @callback {function} [successCallback] Function to be called when SQL statement is executed successfully. Expected signature: function SQLVoidCallback().
@@ -46,7 +48,7 @@ Database ={
 		transaction : function(callback, errorCallback, successCallback){},
 		
 		/**
-		 * @desc When called, this method immediately returns and then asynchronously runs the transaction steps with the <i>transaction callback</i> being the first argument, the <i>error callback</i> being the second argument, if any, the <i>success callback</i> being the third argument, if any, and with no <i>preflight operation</i> or <i>postflight operation</i>. The mode is read-only.
+		 * @description When called, this method immediately returns and then asynchronously runs the transaction steps with the <i>transaction callback</i> being the first argument, the <i>error callback</i> being the second argument, if any, the <i>success callback</i> being the third argument, if any, and with no <i>preflight operation</i> or <i>postflight operation</i>. The mode is read-only.
 		 * @callback {function} callback Function to be called when executing SQL statements. Expected signature: function SQLTransactionCallback(SQLTransaction transaction).
 		 * @callback {function} [errorCallback] Function to be called when SQL error occurs. Expected signature: function SQLTransactionErrorCallback(SQLError error).
 		 * @callback {function} [successCallback] Function to be called when SQL statement is executed successfully. Expected signature: function SQLVoidCallback().
@@ -73,9 +75,9 @@ Database ={
 		readTransaction : function(callback, errorCallback, successCallback){},
 		
 		/**
-		 * @desc This method allows scripts to atomically verify the version number and change it at the same time as doing a schema update. When the method is invoked, it immediately returns, and then asynchronously runs the transaction steps with the <i>transaction callback</i> being the third argument, the <i>error callback</i> being the fourth argument, the <i>success callback</i> being the fifth argument. If any of the optional arguments are omitted, then they are treated as if they were null.
-		 * @param  {DOMString} oldVersion database's current version.
-		 * @param  {DOMString} newVersion database's new version.
+		 * @description This method allows scripts to atomically verify the version number and change it at the same time as doing a schema update. When the method is invoked, it immediately returns, and then asynchronously runs the transaction steps with the <i>transaction callback</i> being the third argument, the <i>error callback</i> being the fourth argument, the <i>success callback</i> being the fifth argument. If any of the optional arguments are omitted, then they are treated as if they were null.
+		 * @param  {String} oldVersion database's current version.
+		 * @param  {String} newVersion database's new version.
 		 * @callback {function} [callback] Function to be called when executing SQL statements. Expected signature: function SQLTransactionCallback(SQLTransaction transaction).
 		 * @callback {function} [errorCallback] Function to be called when SQL error occurs. Expected signature: function SQLTransactionErrorCallback(SQLError error).
 		 * @callback {function} [successCallback] Function to be called when SQL statement is executed successfully. Expected signature: function SQLVoidCallback().
@@ -98,8 +100,8 @@ Database ={
  */
 SQLTransaction = {
 		/**
-		 * @desc This method executes the provided SQL statement
-		 * @param {DOMString} sqlStatement the SQL statement
+		 * @description This method executes the provided SQL statement
+		 * @param {String} sqlStatement the SQL statement
 		 * @param {ObjectArray} [arguments] the optional arguments used to preprocess a SQL statement by binding each ? placeholder with the value of the argument in the arguments array with the same position.
 		 * @param {function} [callback] Function to be called when statement's result set is ready. Expected signature: function SQLStatementCallback (SQLTransaction transaction, SQLResultSet resultSet).
 		 * @param {function} [errorCallback] Function to be called when SQL error occurs. Expected signature: function SQLStatementErrorCallback (SQLTransaction transaction, SQLError error).
@@ -136,25 +138,25 @@ SQLTransaction = {
  */
 SQLResultSet = {
 		/**
-		 * @desc If the SQL statement inserted a row, this attribute returns the row ID of the row that the SQLResultSet object's SQL statement inserted into the database. If the SQL statement inserted multiple rows, this attribute returns the the ID of the last row. 
+		 * @description If the SQL statement inserted a row, this attribute returns the row ID of the row that the SQLResultSet object's SQL statement inserted into the database. If the SQL statement inserted multiple rows, this attribute returns the the ID of the last row. 
 		 * @readOnly
-		 * @type long
+		 * @type Number
 		 * @PB10
 		 * @BB50+
 		 */
 		insertId : 0,
 		
 		/**
-		 * @desc This attribute returns the number of rows that were changed by the SQL statement. If the statement did not affected any rows, 0 is returned.
+		 * @description This attribute returns the number of rows that were changed by the SQL statement. If the statement did not affected any rows, 0 is returned.
 		 * @readOnly
-		 * @type long
+		 * @type Number
 		 * @PB10
 		 * @BB50+
 		 */
 		rowsAffected : 0,
 
 		/**
-		 * @desc This attribute represents the rows returned in the order returned by the database.If no rows were returned, then the object will be empty (its <i>length</i> will be zero).
+		 * @description This attribute represents the rows returned in the order returned by the database.If no rows were returned, then the object will be empty (its <i>length</i> will be zero).
 		 * @readOnly
 		 * @type SQLResultSetRowList
 		 * @PB10
@@ -174,17 +176,17 @@ SQLResultSet = {
  */
 SQLResultSetRowList = {
 		/**
-		 * @desc the number of rows returned by the database.
+		 * @description the number of rows returned by the database.
 		 * @readOnly
-		 * @type unsigned long
+		 * @type Number
 		 * @PB10
 		 * @BB50+
 		 */
 		length : 0,
 		
 		/**
-		 * @desc return the row with the given index.
-		 * @param {unsigned long} index the index
+		 * @description return the row with the given index.
+		 * @param {Number} index the index
 		 * @returns {Object} the row with the given index. If there is no such row, return null.
 		 * @PB10
 		 * @BB50+
@@ -204,7 +206,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc The transaction failed for reasons unrelated to the database itself and not covered by any other error code. 
+		 * @description The transaction failed for reasons unrelated to the database itself and not covered by any other error code. 
 		 * @PB10
 		 * @BB50+
 		 * @default 0
@@ -214,7 +216,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc The statement failed for database reasons not covered by any other error code. 
+		 * @description The statement failed for database reasons not covered by any other error code. 
 		 * @PB10
 		 * @BB50+
 		 * @default 1
@@ -224,7 +226,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc The operation failed because the actual database version was not what it should be. 
+		 * @description The operation failed because the actual database version was not what it should be. 
 		 * @PB10
 		 * @BB50+
 		 * @default 2
@@ -234,7 +236,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc The statement failed because the data returned from the database was too large. The SQL "LIMIT" modifier might be useful to reduce the size of the result set. 
+		 * @description The statement failed because the data returned from the database was too large. The SQL "LIMIT" modifier might be useful to reduce the size of the result set. 
 		 * @PB10
 		 * @BB50+
 		 * @default 3
@@ -244,7 +246,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc The statement failed because there was not enough remaining storage space, or the storage quota was reached and the user declined to give more space to the database. 
+		 * @description The statement failed because there was not enough remaining storage space, or the storage quota was reached and the user declined to give more space to the database. 
 		 * @PB10
 		 * @BB50+
 		 * @default 4
@@ -254,7 +256,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc The statement failed because of a syntax error, or the number of arguments did not match the number of ? placeholders in the statement, or the statement tried to use a statement that is not allowed, such as <b>BEGIN</b>, <b>COMMIT</b>, or <b>ROLLBACK</b>, or the statement tried to use a verb that could modify the database but the transaction was read-only. 
+		 * @description The statement failed because of a syntax error, or the number of arguments did not match the number of ? placeholders in the statement, or the statement tried to use a statement that is not allowed, such as <b>BEGIN</b>, <b>COMMIT</b>, or <b>ROLLBACK</b>, or the statement tried to use a verb that could modify the database but the transaction was read-only. 
 		 * @PB10
 		 * @BB50+
 		 * @default 5
@@ -264,7 +266,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc An <b>INSERT</b>, <b>UPDATE</b>, or <b>REPLACE</b> statement failed due to a constraint failure. For example, because a row was being inserted and the value given for the primary key column duplicated the value of an existing row. 
+		 * @description An <b>INSERT</b>, <b>UPDATE</b>, or <b>REPLACE</b> statement failed due to a constraint failure. For example, because a row was being inserted and the value given for the primary key column duplicated the value of an existing row. 
 		 * @PB10
 		 * @BB50+
 		 * @default 6
@@ -274,7 +276,7 @@ SQLError = {
 		/**
 		 * @constant
 		 * @type Number
-		 * @desc A lock for the transaction could not be obtained in a reasonable time. 
+		 * @description A lock for the transaction could not be obtained in a reasonable time. 
 		 * @PB10
 		 * @BB50+
 		 * @default 7
@@ -282,18 +284,18 @@ SQLError = {
 		TIMEOUT_ERR : 7,
 
 		/**
-		 * @desc the most appropriate error code 
+		 * @description the most appropriate error code 
 		 * @readOnly
-		 * @type unsigned short
+		 * @type Number
 		 * @PB10
 		 * @BB50+
 		 */
 		code : 0,
 
 		/**
-		 * @desc an error message describing the error encountered. The message should be localized to the user's language.
+		 * @description an error message describing the error encountered. The message should be localized to the user's language.
 		 * @readOnly
-		 * @type DOMString
+		 * @type String
 		 * @PB10
 		 * @BB50+
 		 */
