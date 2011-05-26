447bbe11611ea53cc509c9225ff3f7ce99bc0646
Sergey Golod <sgolod@rim.com>
2011-05-26 12:33:04 -0400

    Payment API cleaned up and corrected.

---
+++ api/blackberry_payment.js
@@ -0,0 +1,181 @@
+/*
+* Copyright 2010-2011 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ * @namespace
+ * Entry point for purchasing digital goods.
+ * <p>
+ * API that permits BlackBerry device users to initiate the purchase of digital goods from within your application. For example, this API can be used to allow users to purchase additional levels in a gaming application, music from a radio application, or any other digital good registered on the Vendor Portal for BlackBerry App World. The digital good being purchased must be associated with the calling application in the Vendor Portal for BlackBerry App World.
+ * <p>
+ * Purchases are initiated via the purchase method. The amount of time that elapses before a response is returned depends on how quickly the user completes the purchase process (which may include steps such as signing in to their BlackBerry ID account and setting up their preferred billing method, etc.). The purchase method dispatches a PaymentSuccessEvent on success, or dispatches a PaymentErrorEvent on failure.
+ * <p>
+ * When calling the puchase method only the ID or SKU of the digital good to be purchased is required; it is not necessary to provide both, and all other arguments are optional. If both the ID and SKU are provided, then the ID takes precedence; the SKU is only used if the digital good could not be located on the Payment Service server based on the ID.
+ * <p>
+ * If an application requires a list of its digital goods that have already been purchased by the user (e.g., to avoid offering for sale a digital good the user already owns), such a list can be obtained with the getExistingPurchases method. This method requires the same user interaction as the purchase method, so it can also be a long-running method.
+ * @toc {Payment} Payment
+ * @featureID blackberry.payment
+ */
+blackberry.payment = {
+
+    /**
+     * @function
+     * @description Retrieves the previous successful purchases made by the user from within the calling application.
+     * @param {Boolean} [refresh]  True if the BlackBerry should be allowed to refresh the list of purchases from the Payment Service server. False if the current list of cached purchases should be returned immediately.
+     * @callback {function} callbackOnSuccess Function to be invoked on successful call.
+     * @callback {Purchase[]} callbackOnSuccess.data An array of purchases is passed as a parameter in the form below.
+     * <pre>[{
+     * "transactionID": "00000001",
+     * "digitalGoodID": "123",
+     * "date": "1234567891011",
+     * "digitalGoodSKU": "SKU 1",
+     * "licenseKey": null,
+     * "metaData": ""
+     * },
+     * {
+     * "transactionID": "00000002",
+     * "digitalGoodID": "456",
+     * "date": "1234567891011",
+     * "digitalGoodSKU": "SKU 2",
+     * "licenseKey": null,
+     * "metaData": ""
+     * }]</pre>
+     * @callback {function} [callbackOnFailure] Function to be invoked when an error occurs.
+     * @callback {Number} callbackOnFailure.error An error code will be passed in corresponding to the following codes
+     * <ul>
+     * <li> User Cancelled = 1</li>
+     * <li> Payment System Busy = 2</li>
+     * <li> General Payment System Error  = 3</li>
+     * <li> Digital Good not Found = 4</li>
+     * </ul>
+     * @PB10
+     */
+    getExistingPurchases : function (refresh, callbackOnSuccess, callbackOnFailure) {
+    },
+    /**
+     * @function
+     * @description Initiates the purchase of a digital good.
+     * <p>
+     * Only the ID or SKU of the digital good to be purchased is required; it is not necessary to provide both. If both the ID and SKU are provided, then the ID takes precedence; the SKU is only used if the digital good cannot be located by the Payment Service based on the ID.
+     * @param {Object} args Contains an information that describes the purchase.
+     * @callback {String} args.digitalGoodID ID of the digital good being purchased.
+     * @callback {String} args.digitalGoodSKU SKU of the digital good being purchased.
+     * @callback {String} args.digitalGoodName Name of the digital good being purchased.
+     * @callback {String} args.metaData Metadata associated with the purchase being purchased.
+     * @callback {String} args.purchaseAppName Name of the application requesting the purchase.
+     * @callback {String} args.purchaseAppIcon Icon of the application requesting the purchase.
+     * @callback {function} callbackOnSuccess Function to be called when the payment is successful.
+     * @callback {Purchase} callbackOnSuccess.data An object representing a purchase is passed as a parameter in the form below.
+     * @callback {function} [callbackOnFailure] Function to be called when an error occurs.
+     * @callback {Number} callbackOnFailure.error An error code will be passed in corresponding to the following codes
+     * <ul>
+     * <li> User Cancelled = 1</li>
+     * <li> Payment System Busy = 2</li>
+     * <li> General Payment System Error  = 3</li>
+     * <li> Digital Good not Found = 4</li>
+     * </ul>
+     * @PB10
+     * @example
+     * &lt;script type="text/javascript"&gt;
+     *   function pay() {
+     *     try{
+     *       blackberry.payment.purchase({
+     *       "digitalGoodID":"123",
+     *       "digitalGoodSKU":"someSKU",
+     *       "digitalGoodName":"SomeName",
+     *       "metaData":"metadata",
+     *       "purchaseAppName":"WebWorks APP",
+     *       "purchaseAppIcon":null},
+     *       success,failure);
+     *    }catch (e){
+     *      alert ("Error" + e);
+     *    }
+     *  }
+     *
+     *  function success(purchase) {
+     *    alert ("success called:"+ JSON.stringify(purchase));
+     *   }
+     *
+     *  function failure(error) {
+     *    alert ("failure called with error code:"+ error);
+     *  }
+     * &lt;/script&gt;
+     */
+    purchase : function ( args, callbackOnSuccess, callbackOnFailure) {
+    },
+    /**
+     * @type Boolean
+     * @description Sets the development mode used in the application. If development mode is set to true, the application does not contact the Payment Service server for any transactions. For purchases, a simulated purchase screen is displayed, allowing the user to choose the result of the purchase. For retrieving existing purchases, only simulated successful purchases are returned. This mode is useful for testing how your application handles the possible results without requiring network connections or currency. THIS MODE SHOULD NOT BE USED IN PRODUCTION CODE. If development mode is set to false, purchases and retrievals of existing purchases proceed normally, contacting the Payment Service server as necessary. This is the default development mode, and applications in production should not modify it.
+     * @PB10
+     */
+    developmentMode: false
+
+};
+
+/**
+ * @class
+ * @toc {Payment} Purchase
+ */
+Purchase = {
+
+    /**
+     * @type String
+     * @description The ID corresponding to the purchase transaction.
+     * @BB50+
+     * @PB10
+     */
+    transactionID : null,
+
+    /**
+     * @type String
+     * @description The ID corresponding to the digitalGood to be purchased.
+     * @BB50+
+     * @PB10
+     */
+    digitalGoodID : null,
+
+    /**
+     * @type String
+     * @description The SKU corresponding to the digitalGood to be purchased.
+     * @BB50+
+     * @PB10
+     */
+    digitalGoodSKU : null,
+
+    /**
+     * @type String
+     * @description Metadata offers the application developer a way to store information about each purchase on the Payment Service server, and to retrieve that data via net.rim.blackberry.api.payment.PaymentSystem.getExistingPurchases(boolean). For example, assume a book vendor offers many titles at a single price point, and represents them on the vendor portal as a single digital good. In this case, the ISBN of the book can be provided as metadata, which uniquely identifies the digital good that was purchased. The entire list of purchased books can then be retrieved at any time by obtaining previous purchases via the getExistingPurchases method, filtering on the book's digital good Content ID, and finally enumerating the ISBNs in the metadata of each purchase.
+     * @BB50+
+     * @PB10
+     */
+    metaData : null,
+
+    /**
+     * @type String
+     * @description Represents the date this purchase was made.
+     * @BB50+
+     * @PB10
+     */
+    date : null,
+
+    /**
+     * @type String
+     * @description Represents the license key for this purchase, or null if the purchased digital good does not have a license key.
+     * @BB50+
+     * @PB10
+     */
+    licenseKey : null
+
+};
