30f1ab4863cb9ec5ef2ac449448214b1b423fb3c
Jeffrey Heifetz <jheifetz@rim.com>
2011-05-30 13:33:45 -0400

    Fixing bbPlugin

--- build/bbPlugin.js
+++ build/bbPlugin.js
@@ -20,17 +20,16 @@
  * Put this file in <JSDoc dir>\app\plugins\ and it will be used whenever JSDoc is run.
  */
 
-
 BBTag = {};
 
-BBTag.Support = function (symbolArray) {
+BBTag.Support = function(symbolArray) {
     this.init();
     if(symbolArray){
         this.populateBySymbolArray(symbolArray);
     }
 };
 
-BBTag.Support.prototype.init = function () {
+BBTag.Support.prototype.init = function() {
     this.bb50 = false;
     this.bb60 = false;
     this.pb10 = false;
@@ -38,63 +37,63 @@ BBTag.Support.prototype.init = function () {
     this.clearSupportAttributes();
 };
 
-BBTag.Support.prototype.clearSupportAttributes = function () {
+BBTag.Support.prototype.clearSupportAttributes = function() {
     this.supportStrings = [];
     this.supportTag = "";
     this.supportTable = "";
 }
 
-BBTag.Support.prototype.getSupportStrings = function () {
-    if (!this.supportStrings.length) {
-        if (this.bb50 && this.bb60) {
+BBTag.Support.prototype.getSupportStrings = function() {
+    if(!this.supportStrings.length){
+        if(this.bb50 && this.bb60){
             this.supportStrings.push("BlackBerry OS 5.0+");
-        } else if (this.bb50 && !this.bb60) {
+        }else if(this.bb50 && !this.bb60){
             this.supportStrings.push("BlackBerry OS 5.0");
-        } else if (!this.bb50 && this.bb60) {
+        }else if(!this.bb50 && this.bb60){
             this.supportStrings.push("BlackBerry OS 6.0+");
         } // This last else has no support
-        if (this.pb10) {
+        if(this.pb10){
             this.supportStrings.push("BlackBerry PlayBook");
         }
     }
     return this.supportStrings;
 };
 
-BBTag.Support.prototype.getSupportTag = function () {
-    if (!this.supportTag.length) {
-        if (this.bb50 && this.bb60) {
+BBTag.Support.prototype.getSupportTag = function() {
+    if(!this.supportTag.length){
+        if(this.bb50 && this.bb60){
             this.supportTag = "bb5.0|bb6.0";
-        } else if (this.bb50 && !this.bb60) {
+        }else if(this.bb50 && !this.bb60){
             this.supportTag = "bb5.0";
-        } else if (!this.bb50 && this.bb60) {
+        }else if(!this.bb50 && this.bb60){
             this.supportTag = "bb6.0";
         } // This last else has no support
-        if (this.pb10) {
-            if (this.supportTag.length) {
+        if(this.pb10){
+            if(this.supportTag.length){
                 this.supportTag += "|";
             }
             this.supportTag += "pb1.0";
         }
 
-        if (this.common) {
+        if(this.common){
             this.supportTag += "|common";
         }
     }
     return this.supportTag;
 };
 
-BBTag.Support.prototype.getSupportTable = function () {
-    if (!this.supportTable.length) {
+BBTag.Support.prototype.getSupportTable = function() {
+    if(!this.supportTable.length){
         var tableYes = "<td class=\"apiTd apiYes\">Y</td>";
         var tableNo = "<td class=\"apiTd apiNo\">&nbsp;</td>";
-        this.supportTable = (this.bb50?tableYes:tableNo) + "\n" 
-        + (this.bb60?tableYes:tableNo) + "\n" 
-        + (this.pb10?tableYes:tableNo);
+        this.supportTable = (this.bb50 ? tableYes : tableNo) + "\n"
+        + (this.bb60 ? tableYes : tableNo) + "\n"
+        + (this.pb10 ? tableYes : tableNo);
     }
     return this.supportTable;
 };
 
-BBTag.Support.prototype.populateByBools = function (bb50, bb60, pb10) {
+BBTag.Support.prototype.populateByBools = function(bb50, bb60, pb10) {
     this.bb50 |= bb50;
     this.bb60 |= bb60;
     this.pb10 |= pb10;
@@ -102,12 +101,12 @@ BBTag.Support.prototype.populateByBools = function (bb50, bb60, pb10) {
     this.clearSupportAttributes();
 };
 
-BBTag.Support.prototype.populateBySymbol = function (symbol) {
+BBTag.Support.prototype.populateBySymbol = function(symbol) {
     // BlackBerry Support Tags
-    if (symbol) {
-        if (symbol.support) {
+    if(symbol){
+        if(symbol.support){
             this.populateBySupport(symbol.support);
-        } else if (symbol.comment) {
+        }else if(symbol.comment){
             var BB50 = symbol.comment.getTag("BB50").length;
             var BB50P = symbol.comment.getTag("BB50+").length;
             var BB60 = symbol.comment.getTag("BB60").length;
@@ -116,13 +115,14 @@ BBTag.Support.prototype.populateBySymbol = function (symbol) {
             var PB10P = symbol.comment.getTag("PB10+").length;
 
             symbol.support = new BBTag.Support();
-            symbol.support.populateByBools((BB50 || BB50P), (BB50P || BB60P || BB60), (PB10 || PB10P));
+            symbol.support.populateByBools((BB50 || BB50P),
+                    (BB50P || BB60P || BB60), (PB10 || PB10P));
             this.populateBySupport(symbol.support);
         }
     }
 };
 
-BBTag.Support.prototype.populateBySupport = function (support) {
+BBTag.Support.prototype.populateBySupport = function(support) {
 
     this.bb50 |= support.bb50;
     this.bb60 |= support.bb60;
@@ -131,130 +131,130 @@ BBTag.Support.prototype.populateBySupport = function (support) {
     this.clearSupportAttributes();
 };
 
-BBTag.Support.prototype.populateBySymbolArray = function (symbolArray) {
-    for (var i = 0, l = symbolArray.length; i < l; i++) {
+BBTag.Support.prototype.populateBySymbolArray = function(symbolArray) {
+    for ( var i = 0, l = symbolArray.length; i < l; i++){
         var symbol = symbolArray[i];
         this.populateBySymbol(symbol);
     }
 };
 
-
-
 function isaClass($) {
     return ($.is("CONSTRUCTOR") || $.isNamespace) && !($.alias == "_global_")
 }
-
 
 /** Fetch and remove additional { foo } parameters from a string * */
 function GetType(src) {
     var type = null;
 
-    if (typeof src != "string") throw "src must be a string not " + (typeof src);
+    if(typeof src != "string")
+        throw "src must be a string not " + (typeof src);
 
-    if (src.indexOf('{') >= 0) {
+    if(src.indexOf('{') >= 0){
         var typeRange = src.balance("{", "}");
-        if (typeRange[1] == -1) {
-            throw "Malformed comment tag ignored. Tag type requires an opening { and a closing }: " + src;
-        }
+        if(typeRange[1] == -1)
+            throw "Malformed comment tag ignored. Tag type requires an opening { and a closing }: "
+            + src;
         type = src.substring(typeRange[0] + 1, typeRange[1]).trim();
         src = src.substring(typeRange[1] + 1);
     }
 
-    return {type: type, remainder: src};
+    return {type : type, remainder : src};
 }
 
-JSDOC.PluginManager.registerPlugin("JSDOC.BBTag", {
-    onSymbol: function (symbol) {
-        if (symbol.comment) {
-            
+JSDOC.PluginManager.registerPlugin("JSDOC.BBTag",{
+    onSymbol : function(symbol) {
+        if(symbol.comment){
             // If its a class/namespace
-            if (isaClass(symbol)) {
-                
+            if(isaClass(symbol)){
+
                 var toc = symbol.comment.getTag("toc");
-                if (toc.length) {
+                if(toc.length){
                     // MUST get the original TOC tag ...WHO KNOWS WHY???
-                    for (var i = 0; i < symbol.comment.tags.length; i++) {
-                        if (symbol.comment.tags[i].title == "toc") {
+                    for ( var i = 0; i < symbol.comment.tags.length; i++){
+                        if(symbol.comment.tags[i].title == "toc"){
                             symbol.toc = symbol.comment.tags[i];
                             break;
                         }
                     }
                 }
-    
+
                 var notice = symbol.comment.tags.filter(function($, index){$.itemIndex = index; return $.title=="notice" && $.type});
-                if(notice.length) {
+                if(notice.length){
                     // reparse the .type attribute as jsDocs as modifies characters
-                    for(var i = 0; i < notice.length; i++) {
+                    for ( var i = 0; i < notice.length; i++){
                         var n = notice[i];
                         var parts = GetType(symbol.comment.tagTexts[n.itemIndex]);
                         if(parts && parts.type){
                             n.title = parts.type;
                         }
                         n.desc = parts.remainder;
-                    } 
+                    }
                     symbol.notice = notice;
                 }
-    
+
                 var constructorTag = symbol.comment.getTag("constructor");
-                if(constructorTag.length == 0) {
+                if(constructorTag.length == 0){
                     symbol.noConstructor = true;
                 }
-                
+
                 var featureID = symbol.comment.getTag("featureID");
-                if(featureID.length) {
+                if(featureID.length){
                     symbol.featureID = featureID;
                 }
-                
+
                 var permission = symbol.comment.getTag("permission");
-                if(permission.length) {					    
+                if(permission.length){
                     symbol.permission = permission;
                 }
-    
+
                 var betaTag = symbol.comment.getTag("beta");
-                if (betaTag.length) {
+                if(betaTag.length){
                     symbol.betaTag = betaTag;
                 }
-    
+
                 var learnTag = symbol.comment.getTag("learns");
-                if (learnTag.length) {
+                if(learnTag.length){
                     symbol.learnTag = learnTag;
                 }
-            } else { // Its a property or method
-                
+            }else{ // Its a property or method
+
                 var readOnly = symbol.comment.getTag("readOnly");
-                if (readOnly.length) {
+                if(readOnly.length){
                     symbol.readOnly = readOnly;
                 }
-                
+
                 var uri = symbol.comment.getTag("uri");
-                if (uri.length) {
+                if(uri.length){
                     symbol.uri = uri;
                 }
-                
+
                 var squareAccessor = symbol.comment.getTag("squareAccessor");
-                if (squareAccessor.length) {
+                if(squareAccessor.length){
                     symbol.squareAccessor = squareAccessor;
                 }
-                
+
                 var constructedBy = symbol.comment.getTag("constructedBy");
                 if(constructedBy.length){
                     symbol.constructedBy = constructedBy;
-                }                
+                }
             }
-    
+
             var paramCallbacks = symbol.comment.tags.filter(function($){return $.isCallback && $.title == "param"});
             // Mark all parameters as callback based on their tags
-            if (paramCallbacks.length) {
-                for (var i = 0; i < paramCallbacks.length; i++) {
+            if(paramCallbacks.length){
+                for ( var i = 0; i < paramCallbacks.length; i++){
                     var currentCallback = paramCallbacks[i];
                     // There should only be 1 matching param
-                    var matchingParams = symbol.params.filter(function($){return $.type == currentCallback.type &&
-                        $.name == currentCallback.name &&
-                        $.desc == currentCallback.desc &&
-                        $.isOptional == currentCallback.isOptional &&
-                        $.defaultValue == currentCallback.defaultValue});
+                    var matchingParams = symbol.params
+                    .filter(function($) {
+                        return ($.type == currentCallback.type)
+                        && ($.name == currentCallback.name)
+                        && ($.desc == currentCallback.desc)
+                        && ($.isOptional == currentCallback.isOptional)
+                        && ($.defaultValue == currentCallback.defaultValue)
+                    });
                     // Mark the param a callback
-                    if (matchingParams && matchingParams[0]) {
+                    if(matchingParams && matchingParams[0]){
                         matchingParams[0].isCallback = true;
                     }
                 }
@@ -262,25 +262,23 @@ JSDOC.PluginManager.registerPlugin("JSDOC.BBTag", {
     
             var fieldCBs = symbol.comment.tags.filter(function($){return $.isCallback && $.title == "field"});
             // Mark all properties as callback based on their tags
-            if (fieldCBs.length) {
+            if(fieldCBs.length){
                 symbol.isCallback = true;
-                for (var i = 0; i < fieldCBs; i++) {
+                for ( var i = 0; i < fieldCBs; i++){
                     currentCB = fieldCBs[i];
-                    if (currentCB) {}
                 }
             }
         }
     },
 
-
-    onDocCommentTags: function (comment) {
-        if (comment) {
+    onDocCommentTags : function(comment) {
+        if(comment){
             //The name must be nibbled so we get a name property like a normal param
             // They are marked as callbacks for future processing.
             // We mark the items as fields and set the description accordingly
             var propertyCBTags = comment.tags.filter(function($){return $.title == "propertyCB"});
-            if (propertyCBTags.length) {
-                for (var i = 0; i < propertyCBTags.length; i++) {
+            if(propertyCBTags.length){
+                for ( var i = 0; i < propertyCBTags.length; i++){
                     var currentPropertyCBTag = propertyCBTags[i];
                     currentPropertyCBTag.desc = currentPropertyCBTag.nibbleName(currentPropertyCBTag.desc);
                     currentPropertyCBTag.isCallback = true;
@@ -291,46 +289,43 @@ JSDOC.PluginManager.registerPlugin("JSDOC.BBTag", {
                     comment.tags.push(new JSDOC.DocTag("type " + currentPropertyCBTag.type));
                     comment.tags.push(new JSDOC.DocTag("desc " + currentPropertyCBTag.desc));
                 }
-            } else if (comment.getTag("squareAccessor").length) {
+            }else if(comment.getTag("squareAccessor").length){
                 // Push a function tag because we only support [] functions
                 comment.tags.push(new JSDOC.DocTag("function"));
             }
         }
     },
 
-    onDocTag: function (docTag) {
-        if (docTag.title) {
+    onDocTag : function(docTag) {
+        if(docTag.title){
             // Callbacks pretend to be parameters so that their order is preserved
             // The name must be nibbled so we get a name property like a normal param
             // They are marked as callbacks for future processing.
-            if (docTag.title == "callback") {
+            if(docTag.title == "callback"){
                 docTag.desc = docTag.nibbleName(docTag.desc);
                 docTag.title = "param";
                 docTag.isCallback = true;
-            } else if (docTag.title == "learns") {
+            }else if(docTag.title == "learns"){
                 docTag.desc = docTag.nibbleName(docTag.desc);
-            }          
+            }
         }
     },
 
-    onFinishedParsing : function(symbolSet){
+    onFinishedParsing : function(symbolSet) {
         if(symbolSet){
             var symbols = symbolSet.toArray();
             var classes = symbols.filter(isaClass);
-    
             // create each of the class pages
-            for (var i = 0, l = classes.length; i < l; i++) {
+            for ( var i = 0, l = classes.length; i < l; i++){
                 var symbol = classes[i];
                 if(symbol){
                     var symbolSupport = new BBTag.Support();
                     symbolSupport.populateBySymbol(symbol);
-    
                     var children = [].concat(symbol.methods,symbol.properties,symbol.events);
                     var childSupport = new BBTag.Support(children);
                     symbol.support.populateBySupport(childSupport);
                 }
             }
-        }   
+        }
     }
-
 });
