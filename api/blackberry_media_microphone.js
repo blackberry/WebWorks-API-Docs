ebb0886cf77db1b928b5cb470174af9c3dd02a29
Chris DelCol <cdelcol@rim.com>
2011-08-15 20:45:34 -0400

    Microphone api - cherry pick from pull request #241

--- api/blackberry_media_microphone.js
+++ api/blackberry_media_microphone.js
@@ -51,11 +51,11 @@
  *   } else {
  *     $("#pause").button("option", "label", "Pause");
  *   }
- *	
+ *
  *   try {
  *     mic.pause();
  *   } catch (e) {
- *     alert('Pause, e:' + e.message);	
+ *     alert('Pause, e:' + e.message);
  *   }
  * }
  *
@@ -64,9 +64,9 @@
  *     mic.stop();
  *     $("#record").button("option", "disabled", false);
  *     $("#pause").button("option", "disabled", true);
- *     $("#stop").button("option", "disabled", true);		
+ *     $("#stop").button("option", "disabled", true);
  *   } catch (e) {
- *     alert('Stop, e:' + e.message);		
+ *     alert('Stop, e:' + e.message);
  *   }
  * }
  *
@@ -75,20 +75,24 @@
  * }
  *
  * function testError(errorCode, errorMessage) {
- *   alert('error code:' + errorCode + ' error message:' + errorMessage);	
+ *     if(errorMessage == "Unsupported record encoding type") {
+ *         alert("try recording an .amr audio file");
+ *         return;
+ *     }
+ *   alert('error code:' + errorCode + ' error message:' + errorMessage);
  * }
  *
  * &lt;/script&gt;
  * &lt;script type="text/javascript"&gt;
- * $(document).ready(function() {		
+ * $(document).ready(function() {
  *   $("#record").button();
  *   $("#pause").button({ disabled: true });
  *   $("#stop").button({ disabled: true });
- *		
+ *
  *   $("#record").bind("click", record);
  *   $("#pause").bind("click", pause);
- *   $("#stop").bind("click", stop);		
- * });	
+ *   $("#stop").bind("click", stop);
+ * });
  * &lt;/script&gt;
  * &lt;/head&gt;
  * &lt;body&gt;
@@ -100,37 +104,90 @@
  * &lt;/html&gt;
  */
 blackberry.media.microphone = {
-		
-		/**
-		* Records audio from the microphone.<br><br>
-		* The recorded audio file is:
-		* <ul><li>encoded in WAV format</li>
-		* <li>has sampling rate of 44,100 Hz</li>
-		* <li>has 2 channels</li></ul>
-		* @param {String} filePath full path to the file, specified in the form of file:// URL
-		* @callback {function} onCaptured This callback function is called when the application has successfully recorded the audio and saved it to the requested file.
-		* @callback {String} onCaptured.filePath The file path that points to the created WAV file.	
-		* @callback {function} onError This callback function is called if the recording has failed.
-		* @callback {Number} onError.errorCode The error code for the error.
-		* @callback {String} onError.errorMsg The error message for the error.	
-		* @returns {void}
-		* @PB10
-		*/
-		record : function(filePath, onCaptured, onError) {},
-	
-		/**
-		* Pauses/resumes recording.<br><br>If the microphone is in recording mode, calling pause() would cause recording to
-		* pause. If the microphone is paused, calling pause() would cause recording to resume.<br><br>
-		* @returns {void}
-		* @PB10	
-		*/	
-		pause : function() {},
-	
-		/**
-		* Stops recording, this causes the recorded audio data to be saved to the file. The onCaptured callback function passed in record will be
-		* invoked when the audio file has been saved to disk.
-		* @returns {void}
-		* @PB10	
-		*/	
-		stop : function() {}
+        /**
+        * Records audio from the microphone.<br><br>
+        * If the specified format is not supported on the device, an error is passed to the onError callback with the message &quot;Unsupported record encoding type&quot;.
+        * The supported recording formats are:
+        * <table class="confluenceTable">
+        * <tr>
+        * <th class="confluenceTh">&nbsp;</th>
+        * <th class="confluenceTh"><strong>PlayBook</th>
+        * <th class="confluenceTh">BB OS 5.0, 6.0 (GSM)</th>
+        * <th class="confluenceTh">BB OS 5.0, 6.0 (CDMA)</th>
+        * <th class="confluenceTh">BB OS 7.0</th>
+        * <tr>
+        * <th class="confluenceTh">Supported Formats:</th>
+        * <td class="confluenceTd">WAV</td>
+        * <td class="confluenceTd">WAV, AMR</td>
+        * <td class="confluenceTd">AMR</td>
+        * <td class="confluenceTd">WAV, AMR</td>
+        * </tr>
+        * <tr>
+        * <th class="confluenceTh">Bit Depth:</th>
+        * <td class="confluenceTd">16</td>
+        * <td class="confluenceTd">16</td>
+        * <td class="confluenceTd">16</td>
+        * <td class="confluenceTd">16</td>
+        * </tr>
+        * <tr>
+        * <th class="confluenceTh">Sampling Rate:</th>
+        * <td class="confluenceTd">44,100Hz</td>
+        * <td class="confluenceTd">8,000Hz</td>
+        * <td class="confluenceTd">8,000Hz</td>
+        * <td class="confluenceTd">8,000Hz</td>
+        * </tr>
+        * <tr>
+        * <th class="confluenceTh">Channels:</th>
+        * <td class="confluenceTd">1</td>
+        * <td class="confluenceTd">1</td>
+        * <td class="confluenceTd">1</td>
+        * <td class="confluenceTd">1</td>
+        * </tr>
+        * </table>
+        * @param {String} filePath full path to the file, specified in the form of file:// URL
+        * @callback {function} onCaptured This callback function is called when the application has successfully recorded the audio and saved it to the requested file.
+        * @callback {String} onCaptured.filePath The file path that points to the created audio file.
+        * @callback {function} onError This callback function is called if the recording has failed.
+        * @callback {Number} onError.errorCode The error code for the error.
+        * @callback {String} onError.errorMsg The error message for the error.
+        * @returns {void}
+        * @PB10
+        * @BB50+
+        */
+        record : function(filePath, onCaptured, onError) {},
+
+        /**
+        * Pauses/resumes recording.<br><br>If the microphone is in recording mode, calling pause() would cause recording to
+        * pause. If the microphone is paused, calling pause() would cause recording to resume.<br><br>
+        * @returns {void}
+        * @PB10
+        * @BB50+
+        */
+        pause : function() {},
+
+        /**
+        * Stops recording, this causes the recorded audio data to be saved to the file. The onCaptured callback function passed in record will be
+        * invoked when the audio file has been saved to disk.
+        * @returns {void}
+        * @PB10
+        * @BB50+
+        */
+        stop : function() {}
+        
+        /**
+        * Return the mime types that the record method supports<br/><br/>
+        * Sample values: &quot;audio/amr&quot;, &quot;audio/wav&quot;
+        * @returns {String[]}
+        * @BB50+
+        * @example
+        * function printSupportedTypes() {
+        *         var types = blackberry.media.microphone.getSupportedMediaTypes();
+        *         var alertMsg = "";
+        *         for(var i = 0; i < types.length; i++) {
+        *             alertMsg += types[i] + " ";
+        *         }
+        *         alert(alertMsg);
+        * }
+        */
+        getSupportedMediaTypes : function() {}
 };
