4f86e02f25bc42f533777dcf61b914ea6b05af0c
Tim Neil <tneil@rim.com>
2011-09-23 13:42:45 -0400

    added ripple to workers and removed 5.0 warning.. now in v2.1.1

--- api/html5_worker.js
+++ api/html5_worker.js
@@ -21,8 +21,6 @@
  * @param {String} fileName The name of the javascript file to be executed by the worker.
  * @description Creates a new Worker object. 
  * @learns {Sample - Using HTML5 Web Worker } http://supportforums.blackberry.com/t5/Web-and-WebWorks-Development/Sample-Application-Using-HTML5-Web-Workers/ta-p/627078 Sample that demonstrates how to use the HTML5 Web Workers API [BlackBerry Developer Resource Center].
- * @notice {Warning (BlackBerry 5.0 Notice):}
- * Geolocation support on BlackBerry OS 5.0 is accomplished by using the <a href="http://supportforums.blackberry.com/t5/Web-and-WebWorks-Development/Supporting-Gears-using-HTML5-in-BlackBerry-WebWorks-applications/ta-p/557280" target="_blank">HTML5 JavaScript toolkit</a> for BlackBerry OS 5.0.
 */
 Worker = function() {};
 
@@ -31,6 +29,7 @@ Worker = function() {};
     * @param {Object} message A message to post when worker is created.
     * @PB10
     * @BB50+
+	* @RIPPLE
     * @example
     * Main script:
     *
@@ -59,7 +58,8 @@ Worker = function() {};
     * @param {String} [senderId] sender ID.
     * @param {Object} [messageObject] message object when send.
     * @PB10
-    * @BB50+        
+	* @RIPPLE
+    * @BB50+  	
     */
     Worker.prototype.onmessage = function(messageText, senderId, messageObject) {};
     
@@ -68,6 +68,7 @@ Worker = function() {};
      * @description The ApplicationCache object associated with this worker
      * @type ApplicationCache
      * @PB10
+	 * @RIPPLE
      * @BB60+
      */
     Worker.prototype.applicationCache = undefined;
