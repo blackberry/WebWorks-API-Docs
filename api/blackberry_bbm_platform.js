44c86279e843e10204be24d64a435d25b551313a
Ryan Burbidge <ryan.burbidge@gmail.com>
2011-10-26 15:36:03 -0400

    Changed link to BBM getting started guide to a more stable one.

--- api/blackberry_bbm_platform.js
+++ api/blackberry_bbm_platform.js
@@ -37,7 +37,7 @@
  * </ul>
  * @BB50+
  * @learns {Download BBM SDK Resources} http://us.blackberry.com/developers/blackberrymessenger/ Download the resources required to use the BBM SDK for WebWorks [BlackBerry].
- * @learns {Getting Started Guide} http://docs.blackberry.com/en/developers/deliverables/30299/ Setup the BBM SDK for BlackBerry WebWorks [BlackBerry Developer Resource Center].
+ * @learns {Getting Started Guide} http://docs.blackberry.com/en/developers/subcategories/?userType=21&category=BlackBerry+WebWorks+for+Smartphones Setup the BBM SDK for BlackBerry WebWorks [BlackBerry Developer Resource Center].
  * @learns {Sample Application Guide} http://supportforums.blackberry.com/t5/Web-and-WebWorks-Development/Getting-Started-BlackBerry-WebWorks-Development-for-Smartphones/ta-p/1185353 Get started with the sample application included with the BBM SDK for BlackBerry WebWorks [BlackBerry Developer Resource Center].
  */
 blackberry.bbm.platform = {
