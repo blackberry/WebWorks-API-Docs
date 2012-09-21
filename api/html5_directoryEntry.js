28e07ed53c121b513bf532f31f61ebc9b1e282b0
Jeffrey Heifetz <jheifetz@rim.com>
2012-09-21 15:14:41 -0400

    Fixing directory entry which was providing a bogus entry into the toc

--- api/html5_directoryEntry.js
+++ api/html5_directoryEntry.js
@@ -39,12 +39,12 @@ DirectoryEntry = function() {};
     /**
     * @description  Moves a directory to a different location. When moving the directory to a location where the name already exists, <b>moveTo</b> will attempt to overwrite it.
     * @param {DirectoryEntry} The directory that you want to move
-    * @param {String} The new name of the directory. Defaults to the current name if unspecified. 
+    * @param {String} The new name of the directory. Defaults to the current name if unspecified.
     * @callback {successCallback} callback function to execute when it is successful
     * @callback {errorCallback} callback function to execute when there is a failure
     * @PB10+
     * @RIPPLE
-    * @BB10X	
+    * @BB10X
     * @example
     * Main script:
     *
@@ -52,7 +52,7 @@ DirectoryEntry = function() {};
     DirectoryEntry.prototype.moveTo = function(parent, newName, successCallback, errorCallback) {};
 
     /**
-    * @description Copies the directory to a new location. When copying to a location where the name already exists, <b>copyTo</b> will attempt to overwrite it. 
+    * @description Copies the directory to a new location. When copying to a location where the name already exists, <b>copyTo</b> will attempt to overwrite it.
     * @param {DirectoryEntry} original directory
     * @param {String} newName new name of directory
     * @callback {successCallback} callback function to execute when it is successful
@@ -62,7 +62,7 @@ DirectoryEntry = function() {};
     * @BB10X
     */
     DirectoryEntry.prototype.copyTo = function(parent, newName, successCallback, errorCallback) {};
-    
+
     /**
      * @description Returns a URL that can be used to locate the directory.
      * @type string
@@ -73,7 +73,7 @@ DirectoryEntry = function() {};
     DirectoryEntry.prototype.toURL = function() {};
 
     /**
-    * @description function to delete a directory. 
+    * @description function to delete a directory.
     * @callback {successCallback} callback function to execute when it is successful
     * @callback {@callback} callback function to execute when there is a failure
     * @PB10+
@@ -81,7 +81,7 @@ DirectoryEntry = function() {};
     * @BB10X
     */
     DirectoryEntry.prototype.remove = function(successCallback, errorCallback) {};
-		
+
     /**
     * @description  returns the parent DirectoryEntry
     * @callback {successCallback} callback function to execute when it is successful
@@ -91,10 +91,10 @@ DirectoryEntry = function() {};
     * @BB10X
     */
     DirectoryEntry.prototype.getParent = function(successCallback, errorCallback) {};
- 
+
     /**
     * @description  creates a new DirectoryReader to read contents in a directory
-    * @constructor constructor for directoryReader object
+    * @returns {DirectoryReader} The directoryReader object
     * @PB10+
     * @RIPPLE
     * @BB10X
@@ -118,7 +118,7 @@ DirectoryEntry = function() {};
     * @param {String} Path - path of the file to be looked up or created.
     * @param {flags} Options - options to specify whether the file is created if it doesn't exist
     * @callback {successCallback} callback function that is called with a File object. (Function)
-    * @callback {errorCallback} callback function to execute when there is an error during creating the File object. Invoked with a FileError object. 
+    * @callback {errorCallback} callback function to execute when there is an error during creating the File object. Invoked with a FileError object.
     * @PB10+
     * @RIPPLE
     * @BB10X
@@ -128,33 +128,33 @@ DirectoryEntry = function() {};
     /**
     * @description Delete a directory and all of its contents.
     * @callback {successCallback} callback function that is called with a File object. (Function)
-    * @callback {errorCallback} callback function to execute when there is an error during creating the File object. Invoked with a FileError object. 
+    * @callback {errorCallback} callback function to execute when there is an error during creating the File object. Invoked with a FileError object.
     * @PB10+
     * @RIPPLE
     * @BB10X
     */
     DirectoryEntry.prototype.removeRecursively  = function(successCallback, errorCallback) {};
-	
+
     /**
-     * @description Returns whether the object is a file or not. Value is always false. 
+     * @description Returns whether the object is a file or not. Value is always false.
      * @field
      * @type boolean
      * @PB10+
      * @RIPPLE
      * @BB10X
      */
-    DirectoryEntry.isFile = false;	
-	
+    DirectoryEntry.isFile = false;
+
     /**
      * @field
-     * @description Returns whether the object is a directory or not. Value is always true. 
+     * @description Returns whether the object is a directory or not. Value is always true.
      * @type boolean
      * @PB10+
      * @RIPPLE
      * @BB10X
      */
-    DirectoryEntry.isDirectory = true;	
-	
+    DirectoryEntry.isDirectory = true;
+
     /**
      * @field
      * @description {string} The full absolute path from the root to the DirectoryEntry
@@ -164,7 +164,7 @@ DirectoryEntry = function() {};
      * @BB10X
      */
     DirectoryEntry.fullPath = undefined;
-	
+
     /**
      * @field
      * @description {string} The file system on which the DirectoryEntry resides
@@ -174,18 +174,18 @@ DirectoryEntry = function() {};
      * @BB10X
      */
     DirectoryEntry.filesystem = undefined;
-	
-	
+
+
 /**
  * @toc {IO} HTML5 Flags
  * @namespace
- * @description This object is used to indicate the the file or directory should be created or not. 
+ * @description This object is used to indicate the the file or directory should be created or not.
 */
-Flags = function() {};	
-	
+Flags = function() {};
+
     /**
      * @field
-     * @description {boolean} Indicates whether the file or directory should be created or not, if it does not exist. 
+     * @description {boolean} Indicates whether the file or directory should be created or not, if it does not exist.
      * @type boolean
      * @PB10+
      * @RIPPLE
@@ -195,10 +195,10 @@ Flags = function() {};
 
     /**
      * @field
-     * @description {boolean} By itself, exclusive has no effect. Used with create, it causes the file or directory creation to fail if the target path already exists. 
+     * @description {boolean} By itself, exclusive has no effect. Used with create, it causes the file or directory creation to fail if the target path already exists.
      * @type boolean
      * @PB10+
      * @RIPPLE
      * @BB10X
      */
-    Flags.exclusive = undefined;	
+    Flags.exclusive = undefined;
